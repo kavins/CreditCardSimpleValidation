0217eaa78abd58dd043c2a7db50b56865aa796b1
Kavin <kavintec@gmail.com>
2019-02-23 10:33:44 +0530

    Initial commit

---
+++ app/src/test/java/validation/creditcard/com/creditcard_simplevalidation/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package validation.creditcard.com.creditcard_simplevalidation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
